9aedc53931b4a7ed63be10f3e8184208057b698a
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-06-05 19:56:52 +0700

    feat(api): filter products by category

--- ProductApi/Controllers/ProductsController.cs
+++ ProductApi/Controllers/ProductsController.cs
@@ -21,15 +21,18 @@ public class ProductsController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Product>>> GetAll()
     {
-        //Console.WriteLine(">>> GetAll called");
-        return await _context.Products.ToListAsync();
+        return await _context.Products
+            .Include(p => p.Category)
+            .ToListAsync();
     }
 
     // GET: api/products/1
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetById(int id)
     {
-        var product = await _context.Products.FindAsync(id);
+        var product = await _context.Products
+            .Include(p => p.Category)
+            .FirstOrDefaultAsync(p => p.Id == id);
         if (product == null) return NotFound();
         return product;
     }
