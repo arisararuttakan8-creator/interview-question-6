f3bf6dff09928c62385156907f119a63f7fa05e0
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-06-06 11:44:16 +0700

    fix(api): remove duplicate product codes on Railway DB

---
+++ ProductApi/Migrations/20260606044202_FixDuplicateProductCode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDuplicateProductCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM ""Products""
+                WHERE ""Id"" NOT IN (
+                    SELECT MIN(""Id"")
+                    FROM ""Products""
+                    GROUP BY ""Code""
+                )
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
